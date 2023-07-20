cc52097467b26c118ccd4e81e934cf06538cb9b0
Kevin Boehm <kmbhm1@gmail.com>
2023-07-20 12:30:37 -0500

    feat: updated open api docs
    Updated Fast Api's Open API docs.

--- poc_redis_fastapi_chemblntd/chemblntd.py
+++ poc_redis_fastapi_chemblntd/chemblntd.py
@@ -1,4 +1,5 @@
 import datetime
+from pydantic import BaseModel
 
 from redis_om import Field, HashModel
 
@@ -20,7 +21,8 @@ class StrictDate(datetime.date):
         return value
 
 
-class Chembtlntd(HashModel):
+# HashModel for Redis Object Mapper
+class ChembtlntdRedis(HashModel):
     numrow: str
     sid: str = Field(index=True)
     cid: str = Field(index=True)
@@ -40,3 +42,55 @@ class Chembtlntd(HashModel):
     activity_parasite_b_pct: float
     sid_smiles: str = Field(index=True, full_text_search=True)
     pubchem_substance_synonym: str
+
+
+# Model for OpenAPI
+# TODO: conflict occurred while using BaseModel & HashModel metaclasses
+class Chembtlntd(BaseModel):
+    numrow: str
+    sid: str
+    cid: str
+    bioassay_source: str
+    rankscore: int
+    outcome: str
+    depositdate: str
+    luminescence_parasite_a: int
+    luminescence_parasite_b: int
+    luminescence_liver_a: float
+    luminescence_liver_b: float
+    parasite_pct_control_a_pct: float
+    parasite_pct_control_b_pct: float
+    liver_pct_control_a_pct: float
+    liver_pct_control_b_pct: float
+    activity_parasite_a_pct: float
+    activity_parasite_b_pct: float
+    sid_smiles: str
+    pubchem_substance_synonym: str
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "numrow": "1",
+                    "sid": "123456789",
+                    "cid": "123456789.0",
+                    "bioassay_source": "Harvard",
+                    "rankscore": 33,
+                    "outcome": "Inactive",
+                    "depositdate": "2019/12/31",
+                    "luminescence_parasite_a": 0,
+                    "luminescence_parasite_b": 0,
+                    "luminescence_liver_a": 0.0,
+                    "luminescence_liver_b": 0.0,
+                    "parasite_pct_control_a_pct": 0.0,
+                    "parasite_pct_control_b_pct": 0.0,
+                    "liver_pct_control_a_pct": 0.0,
+                    "liver_pct_control_b_pct": 0.0,
+                    "activity_parasite_a_pct": 0.0,
+                    "activity_parasite_b_pct": 0.0,
+                    "sid_smiles": "CC1=CC=C(C=C1)C(=O)NC2=CC=C(C=C2)C(=O)NC3=CC=C(C=C3)C(=O)NC4=CC=C(C=C4)C(=O)NC5=CC=C(C=C5)C(=O)NC6=CC=C(C=C6)C(=O)NC7=CC=C(C=C7)C(=O)NC8=CC=C(C=C8)C(=O)NC9=CC=C(C=C9",
+                    "pubchem_substance_synonym": "PubChem Substance Synonym",
+                }
+            ]
+        }
+    }
--- poc_redis_fastapi_chemblntd/main.py
+++ poc_redis_fastapi_chemblntd/main.py
@@ -1,4 +1,8 @@
+import json
 from fastapi import FastAPI
+from fastapi.params import Path
+from fastapi.responses import JSONResponse
+from pydantic import Field
 import redis
 import pandas as pd
 import tempfile
@@ -6,26 +10,37 @@ import logging
 import os
 import urllib.request
 import uuid
-from poc_redis_fastapi_chemblntd.chemblntd import Chembtlntd
 from redis_om import NotFoundError, Migrator
 
+from poc_redis_fastapi_chemblntd.chemblntd import ChembtlntdRedis
+from poc_redis_fastapi_chemblntd.openapi import (
+    Hashes,
+    Items,
+    Message,
+    SmilesBody,
+    responses,
+    Item,
+)
+
 # logger setup
-logging.basicConfig(level=logging.INFO, format="%(name)s - %(levelname)s - %(message)s")
+logging.basicConfig(
+    level=logging.INFO, format="%(levelname)s:\t  %(name)s - %(message)s"
+)
 
 # get root logger
 logger = logging.getLogger(__name__)
 
-# init Fast Api & Redis
+# init fastapi
 app = FastAPI()
-# for running in docker compose
-REDIS_HOST = os.environ.get("REDIS_HOST", "127.0.0.1")
+
+# init redis
+REDIS_HOST = os.environ.get("REDIS_HOST", "127.0.0.1")  # for docker-compose
 r = redis.Redis(host=REDIS_HOST, port=6379, decode_responses=True)
 
 # Constants
 CSV_URL = "ftp://ftp.ebi.ac.uk/pub/databases/chembl/ChEMBLNTD/set7_harvard_liver/Harvard_ALL.csv"
 TEMP_DIR = tempfile.gettempdir()
 TEMP_FILE_LOC = f"{TEMP_DIR}/{str(uuid.uuid4())}.csv"
-REDIS_OBJ = "chemblntd"
 
 
 def remove_file(path: str):
@@ -36,68 +51,132 @@ def remove_file(path: str):
         pass
 
 
-@app.get("/chemblntd/hash")
-async def get_indices():
-    identifier = "valid hashes"
-    keys = r.keys()
-    hashes = []
-    if len(keys) != 0:
-        hashes = [k.split(":")[2] for k in keys]
-        hashes.sort()
-
-    return {"chemblntd": identifier, "data": hashes}
-
-
-@app.get("/chemblntd/hash/{hash}")
-async def get_chemblntd(hash: str):
-    try:
-        result = Chembtlntd.get(hash)
-        return result.dict()
-    except NotFoundError:
-        return {}
-
-
-@app.get("/chemblntd/search/cid/{cid}")
-async def search_cid_all(cid: str):
-    try:
-        results = Chembtlntd.find(Chembtlntd.cid == cid).all()
-    except NotFoundError:
-        return []
-
-    return [r.dict() for r in results]
-
-
-@app.get("/chemblntd/search/sid/{sid}")
-async def search_sid_all(sid: str):
-    try:
-        results = Chembtlntd.find(Chembtlntd.sid == sid).all()
-    except NotFoundError:
-        return []
-
-    return [r.dict() for r in results]
-
-
-@app.get("/chemblntd/search/smiles/{smiles}")
-async def search_smiles(smiles: str):
-    try:
-        results = Chembtlntd.find(Chembtlntd.sid_smiles % smiles).all()
-    except NotFoundError:
-        return []
-
-    return [r.dict() for r in results]
-
-
-@app.post("/refresh")
-async def refresh():
-    # ping redis db
+def ping_redis() -> bool:
     try:
         logger.info("Pinging redis db.")
         r.ping()
         logger.info("Redis db is up.")
+        return True
     except redis.exceptions.ConnectionError as e:
         logger.error("Redis db is down. Exiting.")
         logger.error(e)
-        return
+        return False
+
+
+def flush_redis() -> bool:
+    try:
+        logger.info("Flushing redis db.")
+        r.flushdb()
+        return True
+    except Exception as e:
+        logger.error("Error flushing redis db. Exiting.")
+        logger.error(e)
+        return False
+
+
+@app.get(
+    "/chemblntd/hash",
+    response_model=Hashes,
+    responses={500: {"message": "Internal server error"}},
+)
+async def get_hashes():
+    try:
+        keys = r.keys()
+        hashes = []
+        if len(keys) != 0:
+            hashes = [k.split(":")[2] for k in keys]
+            hashes.sort()
+
+        logger.info(f"Number of hashes: {len(hashes)}")
+        msg = {"hashes": hashes}
+
+        return JSONResponse(status_code=200, content=msg)
+    except Exception as e:
+        logger.error(e)
+        return JSONResponse(
+            status_code=500, content={"message": "Internal server error"}
+        )
+
+
+@app.get("/chemblntd/hash/{hash}", response_model=Item, responses={**responses})
+async def get_chemblntd(hash: str = Path(example="01H5T42K2AEA7682MR9XD7Y2S4")):
+    try:
+        result = ChembtlntdRedis.get(hash)
+        msg = {"id": hash, "value": result.dict()}
+
+        return JSONResponse(status_code=200, content=msg)
+    except NotFoundError:
+        return JSONResponse(status_code=404, content={"message": "Item not found"})
+    except Exception as e:
+        logger.error(e)
+        return JSONResponse(
+            status_code=500, content={"message": "Internal server error"}
+        )
+
+
+@app.get("/chemblntd/search/cid/{cid}", response_model=Items, responses={**responses})
+async def search_by_cid(cid: str = Path(example="54735847.0")):
+    try:
+        results = ChembtlntdRedis.find(ChembtlntdRedis.cid == cid).all()
+        msg = {"id": cid, "value": results.dict()}
+
+        return JSONResponse(status_code=200, content=msg)
+    except NotFoundError:
+        return JSONResponse(status_code=404, content={"message": "Item not found"})
+    except Exception as e:
+        logger.error(e)
+        return JSONResponse(
+            status_code=500, content={"message": "Internal server error"}
+        )
+
+
+@app.get("/chemblntd/search/sid/{sid}", response_model=Items, responses={**responses})
+async def search_by_sid(sid: str = Path(example="121363756")):
+    try:
+        results = ChembtlntdRedis.find(ChembtlntdRedis.sid == sid).all()
+        msg = {"id": sid, "value": results.dict()}
+
+        return JSONResponse(status_code=200, content=msg)
+    except NotFoundError:
+        return JSONResponse(status_code=404, content={"message": "Item not found"})
+    except Exception as e:
+        logger.error(e)
+        return JSONResponse(
+            status_code=500, content={"message": "Internal server error"}
+        )
+
+
+# TODO: fix json decode errors for examples like "("
+@app.post("/chemblntd/search/smiles/", response_model=Items, responses={**responses})
+async def search_by_smiles(smiles: SmilesBody):
+    try:
+        results = ChembtlntdRedis.find(ChembtlntdRedis.sid_smiles % smiles.smiles).all()
+        msg = {"id": smiles.smiles, "value": [r.dict() for r in results]}
+
+        return JSONResponse(status_code=200, content=msg)
+    except NotFoundError:
+        return JSONResponse(status_code=404, content={"message": "Item not found"})
+    except Exception as e:
+        logger.error(e)
+        return JSONResponse(
+            status_code=500, content={"message": "Internal server error"}
+        )
+
+
+@app.post(
+    "/refresh",
+    response_model=Message,
+    responses={
+        200: {"message": "Refresh complete. Data loaded into redis db."},
+        500: {"message": "Internal server error"},
+    },
+)
+async def refresh():
+    # ping redis db
+    if not ping_redis():
+        return JSONResponse(
+            status_code=500, content={"message": "Internal server error"}
+        )
 
     # download csv file to init db
     try:
@@ -110,18 +189,21 @@ async def refresh():
         logger.error(e)
         remove_file(info[0])
         remove_file(TEMP_FILE_LOC)
-        return
+        return JSONResponse(
+            status_code=500, content={"message": "Internal server error"}
+        )
     except Exception as e:
         logger.error(f"Error downloading file {CSV_URL}.")
         logger.error(e)
         remove_file(TEMP_FILE_LOC)
-        return
+        return JSONResponse(
+            status_code=500, content={"message": "Internal server error"}
+        )
 
-    # load data into pandas df
+    # load data into pandas df & clean
+    # TODO: exception handling here
     logger.info("Loading data into pandas df.")
     df = pd.read_csv(info[0], header=0, sep=",", index_col=False)
-
-    # clean df column names
     df.columns = (
         df.columns.str.strip()
         .str.lower()
@@ -132,50 +214,50 @@ async def refresh():
     )
     logger.info(f"Dataframe shape: {df.shape}.")
 
-    # clear redis db
+    # flush redis db
+    if not flush_redis():
+        remove_file(TEMP_FILE_LOC)
+        del df
+        return JSONResponse(
+            status_code=500, content={"message": "Internal server error"}
+        )
+
+    # load data into redis db
+    # TODO: exception handling and handle objects not added
+    for line_num, (i, row) in enumerate(df.iterrows()):
+        x = ChembtlntdRedis(**row.to_dict())
+        x.save()
+        if ((line_num + 1) % 100) == 0:
+            logger.info(
+                f"Total rows: {line_num+1}. ({round(100*(line_num + 1)/len(df), 1)})%"
+            )
+    logger.info(f"Final data load into Redis: {len(df)} total rows.")
+
+    # create indices by running Migrate for Redis OM
     try:
-        logger.info("Flushing redis db.")
-        r.flushdb()
+        logger.info("Creating indices.")
+        Migrator().run()
     except Exception as e:
-        logger.error("Error flushing redis db. Exiting.")
+        logger.error("Error creating indices.")
         logger.error(e)
         remove_file(TEMP_FILE_LOC)
-        return
+        del df
+        return JSONResponse(
+            status_code=500, content={"message": "Internal server error"}
+        )
 
-    # load csv file into redis db
-    n, cycle = 0, 0
-    for i, row in df.iterrows():
-        x = Chembtlntd(**row.to_dict())
-        x.save()
-        n += 1
-        if (n % 64) == 0:
-            cycle += 1
-            logger.info(f"{cycle} cycles executed. Total rows: {n}.")
-
-    logger.info(
-        f"Final data load into Redis: {cycle} cycles executed. Total rows: {n}."
-    )
-
-    # create indices
-    # Before running queries, we need to run migrations to set up the
-    # indexes that Redis OM will use. You can also use the `migrate`
-    # CLI tool for this!
-    logger.info("Creating indices.")
-    Migrator().run()
-
-    logger.info("Startup event finished.")
+    logger.info("Refresh complete.")
     remove_file(TEMP_FILE_LOC)
-    return {"status": "ok", "message": f"Data loaded into redis db. Total rows: {n}."}
+    del df
+
+    return JSONResponse(
+        status_code=200,
+        content={"message": f"Refresh complete. Data loaded into redis db."},
+    )
 
 
 @app.on_event("startup")
 async def startup_event():
     # ping redis db
-    try:
-        logger.info("Pinging redis db.")
-        r.ping()
-        logger.info("Redis db is up.")
-    except redis.exceptions.ConnectionError as e:
-        logger.error("Redis db is down. Exiting.")
-        logger.error(e)
+    if not ping_redis():
         return
